0ee180d982d3dce4105af0dedf367e443f70dd37
Wangjin <445899710@qq.com>
2016-12-16 20:05:01 +0800

    1
    1

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="format-detection" content="telephone=no">
+<title>妹子美图官方网站 - 真实的妹子私房照社区</title>
+<meta name="description" content="妹子美图的私房照和自拍福利、大尺度照片、宅男福利" />
+<meta name="keywords" content="妹子,美女,福利,妹子网,女,微拍福利,美妞,妹子,妹子网,妹子的私房照,小组妹子,妹子福利,美女私房照,帖子搜索,请不要害羞"/>
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+<link rel="stylesheet" type="text/css" href="./images/style.css">
+<style>
+html{-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;line-height:1.6}
+body{-webkit-touch-callout:none;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif;background-color:#f3f3f3;line-height:inherit}
+h1,h2,h3,h4,h5,h6{font-weight:400;font-size:16px}
+*{margin:0;padding:0;font-style:normal}
+a{color:#607fa6;text-decoration:none}
+ul, ol{list-style:none;}
+body {
+    padding:10px;
+    background: #E7EAEC;
+    color: #222527;
+    font-family: RobotoDraft,Roboto,"Helvetica Neue",Helvetica,Arial,sans-serif;
+}
+.clearfix{zoom: 1;}
+.clearfix:after{content: ".";display: block;height: 0;width: 0;clear: both;font-size: 0;}
+.panel {
+    background:#fff;
+    border: 0;
+    border-radius: 3px;
+    border: 0;
+    box-shadow: 0 0 1px rgba(0,0,0,.09);padding:15px;text-align:center;
+}
+.nav li{float:left;  padding-right:10px; margin-bottom:15px;}
+.nav li a{border: 1px solid #f97474; padding: 6px 15px;color: #f97474; }
+.nav .active a{background: #f97474;color:#fff;}
+.ilist li{margin-bottom:10px;}
+.ilist img{max-width:100%;border-radius: 3px;}
+.ilist li .box{border:1px solid #dcdcdc; padding:5px;border-radius: 3px;}
+</style>
+</head>
+<body>
+
+<div class="panel"><b style="color: #EB5424;font-size: 1.1em;">妹子美图，真实的妹子私房照社区</b></div>
+<div class="panel" style="margin-top:15px;background: #f5f5f5;border-radius: 3px 3px 0 0;padding:15px 15px 0 15px">
+    <div>
+        <ul class="nav">
+        <li>
+        <a href="">所有</a>
+        </li>
+        <li class="active">
+        <a href="">大胸妹</a>
+        </li>
+        <li>
+        <a href="">小翘臀</a>
+        </li>
+        <li>
+        <a href="">黑丝袜</a>
+        </li>
+        <li>
+        <a href="">美腿控</a>
+        </li>
+        <li>
+        <a href="">有颜值</a>
+        </li>
+        <li>
+        <a href="">大杂烩</a>
+        </li>
+        </ul>
+    </div>
+    <div class=clearfix></div>
+</div>
+<div class="panel" style="padding:10px;margin-top:0;border-radius: 0 0 3px 3px;">
+<div style="background:#fff;">
+    <ul class="ilist">
+        <li><div class="box"><a href=""><img title="【晒】听说解封了…嗯" alt="【晒】听说解封了…嗯" src="http://ww3.sinaimg.cn/bmiddle/0060lm7Tgw1f9m66vwzjxj30dw0aet9k.jpg"></a>
+        <p><a href="">【晒】听说解封了…嗯</a></p>
+        </div></li>
+        <li><div class="box"><a href=""><img title="【晒】听说解封了…嗯" alt="【晒】听说解封了…嗯" src="http://ww3.sinaimg.cn/bmiddle/0060lm7Tgw1f9khd15a45j30dw0idgn2.jpg"></a>
+        <p><a href="">【晒】听说解封了…嗯，睡不着啊，睡不着，谁来解救我！！！</a></p>
+        </div></li>
+        
+        </ul>
+    </div>
+</div>
+<div class="panel" style="margin-top:15px;"><b style="color: #EB5424;font-size: 1.1em;">妹子美图，真实的妹子私房照社区</b><br><b style="font-size: 0.8em;">官方微信：妹子美图 ( Meitu8866 )</b><br><b style="font-size: 0.8em; color: #999;">网站：http://meitu8866.com</b></div>
+</body>
+</html>
